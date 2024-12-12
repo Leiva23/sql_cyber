aa941fc19995bb3d80df2ecfec247c3364c0c7b3
Alex Leiva <alex.leiva@enti.cat>
2024-12-12 09:53:42 +0000

    Nueva version

---
+++ ProyectoFinal/alterpizzeria.sql
@@ -0,0 +1,12 @@
+ALTER TABLE comandes MODIFY num_comanda INT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE pizzes MODIFY id INT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE masses MODIFY id INT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE al·lergogens MODIFY id INT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE tipus_ingredients MODIFY id INT UNSIGNED AUTO_INCREMENT;
+ALTER TABLE ingredients MODIFY id INT UNSIGNED AUTO_INCREMENT;
+
+ALTER TABLE estat MODIFY estat ENUM('en preparacio', 'lliurada', 'cancel·lada');
+
+
+ALTER TABLE pizzes_ingredients 
+MODIFY grams_per_ingredients INT CHECK (grams_per_ingredient BETWEEN 10 AND 30);
